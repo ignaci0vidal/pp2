7c7b681a81709d150c42a1ff2f1bd84c238f9f72
ignacio vidal <ignaciovidal@gmail.com>
2026-04-14 20:43:21 -0300

    Actualizo metodos de Producto

--- Producto.js
+++ Producto.js
@@ -10,5 +10,33 @@ class Producto {
     return this.stock >= cantidad;
   }
 
-  
+  reducirStock(cantidad) {
+    if (cantidad <= 0) {
+      throw new Error("La cantidad a reducir debe ser mayor a 0.");
+    }
+
+    if (!this.hayStock(cantidad)) {
+      throw new Error(`No hay stock suficiente para ${this.descripcion}.`);
+    }
+
+    this.stock -= cantidad;
+  }
+
+  aumentarStock(cantidad) {
+    if (cantidad <= 0) {
+      throw new Error("La cantidad a aumentar debe ser mayor a 0.");
+    }
+
+    this.stock += cantidad;
+  }
+
+  actualizarPrecio(nuevoPrecio) {
+    if (nuevoPrecio <= 0) {
+      throw new Error("El precio debe ser mayor a 0.");
+    }
+
+    this.precio = nuevoPrecio;
+  }
+
+
 }
