976ad08890c88e9f750cb438f2df06b84af97033
ignacio vidal <ignaciovidal@gmail.com>
2026-04-14 22:07:46 -0300

    Agrego infomacion del pdf clase3

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+/* --- 1. SET UP INICIAL (Simulación de BD) --- 
+const prod1 = new Producto(1, "Smartphone", 10000, 10); 
+const clienteAna = new Cliente(1, "Ana López", "ana@mail.com", false);
+const promoVerano = { porcentaje: 5, esValido: () => true }; // Mock de Descuento 
+
+// --- 2. INICIO DEL FLUJO DE COMPRA --- 
+const ordenAna = new OrdenCabecera(101, new Date(), clienteAna); 
+ordenAna.descuento = promoVerano; // Aplica cupón 
+
+// --- 3. SELECCIÓN DE PRODUCTOS (Validación de Stock) --- 
+let cantDeseada = 2; 
+if (prod1.hayStock(cantDeseada)) { 
+  ordenAna.agregarDetalle(new OrdenDetalle(1, prod1, cantDeseada)); 
+  prod1.reducirStock(cantDeseada); // Restamos el inventario 
+  } else { 
+    console.log(`Error: No hay stock para ${prod1.nombre}`); 
+  } 
+    
+// --- 4. CIERRE Y FACTURACIÓN --- 
+ordenAna.calcularTotal(); 
+ordenAna.estado = "pagado"; 
+
+// --- 5. EMISIÓN DEL TICKET --- 
+console.log("=== TICKET DE COMPRA ==="); 
+console.log(`Cliente: ${ordenAna.cliente.nombre}`); 
+console.log(`Subtotal: $${ordenAna.subtotal}`); 
+console.log(`Descuento aplicado: $${ordenAna.descuentoMonto}`); 
+console.log(`TOTAL A PAGAR: $${ordenAna.total}`);
+*/
+
 const Cliente = require("./Cliente");
 const Cupon = require("./Cupon");
 const Producto = require("./Producto");
